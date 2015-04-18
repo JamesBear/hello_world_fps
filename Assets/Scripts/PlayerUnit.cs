26ed9c37d1a33f4c20f5db3baac614d2ebae2c9b
J.B <javaxswing@126.com>
2015-04-18 15:22:54 +0800

    different run animation

---
+++ Assets/Scripts/PlayerUnit.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerUnit : MonoBehaviour {
+
+	CharacterController ctrl;
+	public Vector3 moveDir;
+	public Vector3 faceDir;
+
+	void Awake() {
+		ctrl = GetComponent<CharacterController> ();
+		moveDir = Vector3.zero;
+		faceDir = transform.forward;
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float vx = Input.GetAxis ("Horizontal");
+		float vy = Input.GetAxis ("Vertical");
+		float velocity = 3f;
+		moveDir = (new Vector3 (vx, 0f, vy));
+		if (moveDir.sqrMagnitude > 0.01f)
+			moveDir.Normalize ();
+		Vector3 targetV = moveDir * 3f;
+		ctrl.SimpleMove (targetV);
+	}
+}
---
+++ Assets/Scripts/Tuple.cs
@@ -0,0 +1,17 @@
+﻿
+
+public class Tuple<T1, T2, T3> {
+
+	public T1 m1;
+	public T2 m2;
+	public T3 m3;
+
+	public Tuple() {
+	}
+
+	public Tuple(T1 _m1, T2 _m2, T3 _m3) {
+		m1 = _m1;
+		m2 = _m2;
+		m3 = _m3;
+	}
+}
