a446067b04c5e84bf830b912d16ec4c72fb33b1c
J.B <javaxswing@126.com>
2015-04-18 20:37:35 +0800

    first person perspective

---
+++ Assets/Scripts/CallCenter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CallCenter : MonoBehaviour
+{
+	public static CallCenter instance { get; private set;}
+
+	public PlayerUnit player { get; private set;}
+
+	void Awake() {
+		instance = this;
+		player = gameObject.GetComponentInChildren<PlayerUnit> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour {
+
+	private Vector3 offset;
+	public Vector3 Offset;
+	public bool test;
+
+	// Use this for initialization
+	void Start () {
+		test = false;
+		offset = new Vector3 (0f, 2f, 0.3f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (test) {
+			if (Offset.sqrMagnitude < 0.01f)
+				Offset = offset;
+			offset = Offset;
+		}
+
+		var player = CallCenter.instance.player;
+		Vector3 targetPos = player.transform.position + offset;
+		transform.position = targetPos;
+		transform.forward = player.faceDir;
+		transform.Rotate (30, 0, 0);
+	}
+}
